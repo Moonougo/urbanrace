700c3a634cbec96c6149310ebf0debe1c18ce904
Moonougo <155969532+Moonougo@users.noreply.github.com>
2026-05-09 12:37:42 +0200

    Update send-reservation.js

--- netlify/functions/send-reservation.js
+++ netlify/functions/send-reservation.js
@@ -26,7 +26,7 @@ exports.handler = async (event) => {
       }
     });
 
-    const adminEmail = 'fadpsg@gmail.com';
+    const adminEmail = 'info@urbanrace-taxi-moto.fr';
     const clientEmail = data.email || '';
     const fromEmail = '"Urbanrace Taxi Moto" <info@urbanrace-taxi-moto.fr>';
 
